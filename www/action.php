3791e4dbb2363baeb3721d14997c23859c4c7501
Recitasse <126823957+Recitasse@users.noreply.github.com>
2023-03-09 18:54:40 +0100

    Create action.php

---
+++ www/action.php
@@ -0,0 +1,35 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+include "Includes/mdb.php";
+include "Includes/fonction.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Langue</title>
+  <link rel="stylesheet" href="style.css">
+  <script src='script.js'></script>
+  <link rel="icon" href="Images/anglais.ico"/>
+</head>
+<body>
+<div class='principal'>
+   <?php
+     if(isset($_GET) && !empty($_GET))
+     {
+        if(!empty($_GET['type']))
+        {
+           if($_GET['type'] == 'modif')
+           {
+              modifier($_GET['id']);
+           }
+        }
+     }
+   ?>
+   
+</div>
+</body>
+</html>
