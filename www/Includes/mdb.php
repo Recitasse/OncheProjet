80c86832e8af41cbda09bae3b5e107bf4dfe480f
Recitasse <126823957+Recitasse@users.noreply.github.com>
2023-03-09 18:56:16 +0100

    Create mdb.php

---
+++ www/Includes/mdb.php
@@ -0,0 +1,10 @@
+<?php
+$conn = new mysqli('localhost', 'langue', 'blondinChad', 'langue_anglais');
+
+if (mysqli_connect_error()) {
+    die("Connexion échouée: " . mysqli_connect_error());
+}
+mysqli_set_charset($conn, "utf8");
+
+?>
+
